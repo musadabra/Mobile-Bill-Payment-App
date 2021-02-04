edbc5217331f831e1d4a5001758ec412d316f9ba
musadabra <musadabra@gmail.com>
2021-02-04 21:31:37 +0100

    Updated

--- payment/src/main/java/ng/billpayments/service/UserDetailsServiceImpl.java
+++ payment/src/main/java/ng/billpayments/service/UserDetailsServiceImpl.java
@@ -7,7 +7,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
-class UserDetailsServiceImpl implements UserDetailsService {
+public class UserDetailsServiceImpl implements UserDetailsService {
 
     @Autowired
     private UserRepository userRepository;
