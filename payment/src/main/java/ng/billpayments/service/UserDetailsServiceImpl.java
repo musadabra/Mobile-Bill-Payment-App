71268636987623af10b362c6e7c05b6775684714
musadabra <musadabra@gmail.com>
2021-02-04 21:27:48 +0100

    Create UserDetailsServiceImpl

---
+++ payment/src/main/java/ng/billpayments/service/UserDetailsServiceImpl.java
@@ -0,0 +1,27 @@
+package ng.billpayments.service;
+
+import ng.billpayments.model.User;
+import ng.billpayments.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username)
+            throws UsernameNotFoundException {
+        User user = userRepository.getUserByUsername(username);
+
+        if (user == null) {
+            throw new UsernameNotFoundException("Could not find user");
+        }
+
+        return new MyUserDetails(user);
+    }
+
+}
