0b1625f81f51d0d670c9268852912084f262112c
musadabra <musadabra@gmail.com>
2021-02-05 01:01:29 +0100

    update

--- payment/src/main/java/ng/billpayments/PaymentApplication.java
+++ payment/src/main/java/ng/billpayments/PaymentApplication.java
@@ -2,12 +2,11 @@ package ng.billpayments;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class PaymentApplication {
-
+public class PaymentApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(PaymentApplication.class, args);
     }
-
 }
--- payment/src/main/java/ng/billpayments/controller/RoutesController.java
+++ payment/src/main/java/ng/billpayments/controller/RoutesController.java
@@ -1,8 +1,15 @@
 package ng.billpayments.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 public class RoutesController {
-
+    //LOGIN PAGE
+    @RequestMapping("/")
+    public String login() {
+        return "index";
+    }
 }
