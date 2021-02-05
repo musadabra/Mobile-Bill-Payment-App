301de0bd7b396df2be72d7bf5701a3af27765ff9
musadabra <musadabra@gmail.com>
2021-02-05 06:59:38 +0100

    update admin dashboard

--- payment/src/main/java/ng/billpayments/controller/AdminController.java
+++ payment/src/main/java/ng/billpayments/controller/AdminController.java
@@ -8,8 +8,26 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class AdminController {
 
     //REQUEST PATH: /admin
-    @RequestMapping(value = "")
-    public String admin(){
+    @RequestMapping(value = "/dashboard")
+    public String dashboard(){
         return "admin/dashboard";
+    }
+
+    //REQUEST PATH: /admin/vendor
+    @RequestMapping(value = "/vendor")
+    public String vendor(){
+        return "admin/vendor";
+    }
+
+    //REQUEST PATH: /admin/offer
+    @RequestMapping(value = "/offer")
+    public String offer(){
+        return "admin/offer";
+    }
+
+    //REQUEST PATH: /complain/offer
+    @RequestMapping(value = "/complain")
+    public String complain(){
+        return "admin/complain";
     }
 }
--- payment/src/main/java/ng/billpayments/controller/RoutesController.java
+++ payment/src/main/java/ng/billpayments/controller/RoutesController.java
@@ -5,8 +5,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import java.util.List;
-
 @Controller
 public class RoutesController {
     //LOGIN PAGE
@@ -14,16 +12,11 @@ public class RoutesController {
 //    public String login() {
 //        return "index";
 //    }
+    //URL MAPPING FOR CUSTOM LOGIN PAGE AND LOGOUT
 
     //URL MAPPING FOR CUSTOM LOGIN PAGE AND LOGOUT
     @RequestMapping(value = "/login", method = RequestMethod.GET)
     public String login() {
-//        if (error != null)
-//            model.addAttribute("errorMsg", "Your username and password are invalid.");
-//
-//        if (logout != null)
-//            model.addAttribute("msg", "You have been logged out successfully.");
-
         return "login";
     }
 
